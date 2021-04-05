50e887824e7dbd1e8f8d27317cdf1b824f1c8a26
Juan Munoz <juano.mube@gmail.com>
2021-04-04 22:45:19 -0500

    webdriver provider created

--- GenericProject/Providers/WebDriverProvider.cs
+++ GenericProject/Providers/WebDriverProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace GenericProject.Providers
 {
-    class WebDriverProvider
+    public class WebDriverProvider
     {
+        public IWebDriver WebDriver {get;}
+
+        public WebDriverProvider()
+        {
+
+        }
     }
 }
